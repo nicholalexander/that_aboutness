eefac8fcb16dbfc6c221688cb29aaabc14c9eca8
nichol alexander <nichol.alexander@gmail.com>
2018-02-12 11:38:28 -0500

    waffles.

--- sw.js
+++ sw.js
@@ -14,12 +14,11 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2';
-const RUNTIME = 'runtime-v2';
+const PRECACHE = 'precache-v10';
+const RUNTIME = 'runtime-v10';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'index.html',
   '/assets/main-011518.css'
   // '2018/01/10/Delete-Your-Account/',
   // '2017/12/15/Serve-Jekyll-on-Wifi/',
